7e06948a36da709356949bc2899aec866afa36e9
Jonas Nielsen <dev@jonas92.com>
2016-07-01 22:20:27 +0200

    Login page templating

--- .protected/usr/script/page/login.php
+++ .protected/usr/script/page/login.php
@@ -6,5 +6,6 @@ $nav = require USR_FRAGMENT . 'header_navigation.php';
 
 echo $twig->render('login.html', [
 	'title' => 'Login | ' . SITE_TITLE,
-	'header_navigation' => $nav
+	'header_navigation' => $nav,
+	'form_action' => WEBSITE_URL . '/login/'
 ]);
