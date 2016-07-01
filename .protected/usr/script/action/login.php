1cca5ddd89b3dc1f77144693011848ff2d9f9da0
Jonas Nielsen <dev@jonas92.com>
2016-07-01 23:38:13 +0200

    Login script

--- .protected/usr/script/action/login.php
+++ .protected/usr/script/action/login.php
@@ -1,4 +1,29 @@
-Login
-<pre>
-<?php print_r($_REQUEST) ?>
-</pre>
+<?php
+
+if (!isset($_REQUEST['user'])) {
+	exit('Failed to login: Username not provided!');
+}
+if (!isset($_POST['password'])) {
+	exit('Failed to login: Password not provided');
+}
+
+require_once USR_VENDOR . 'doctrine/bootstrap.php';
+
+// Get user
+$users = $entityManager->getRepository('User');
+$user = $users->findOneBy(['name' => $_REQUEST['user']]);
+
+// Check if user exist
+if (!isset($user)) {
+	exit('Failed to login: User doesn\'t exist');
+}
+
+// Verify password
+if (!password_verify($_POST['password'], $user->GetPass())){
+	exit('Failed to login: Invalid password');
+}
+
+// Login
+session_start();
+$_SESSION['logged-in'][$user->getName()] = true;
+header('Location: ' . WEBSITE_URL . '/user/' . $user->getName());
