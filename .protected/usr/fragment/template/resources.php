91457713b9916bcbbf39ac84765a9c067b41c322
Jonas Nielsen <dev@jonas92.com>
2016-07-04 21:35:02 +0200

    Much frontend

--- .protected/usr/fragment/template/resources.php
+++ .protected/usr/fragment/template/resources.php
@@ -3,7 +3,13 @@
 return function () {
 	return [
 		'css' => [
-			'style' => SRC_CSS . 'style.css'
+			'style' => SRC_CSS . 'style.css',
+			'index' => SRC_CSS . 'index.css',
+			'user' => SRC_CSS .'user.css',
+			'bootstrap' => SRC_CSS .'bootstrap.min.css',
+		],
+		'js' => [
+			'bootstrap' => SRC_JS . 'bootstrap.min.js'
 		]
 	];
 };
