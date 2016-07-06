17497d84f65d7a63f2572823e0aaf6f8d8180e01
Jonas Nielsen <dev@jonas92.com>
2016-07-06 23:20:59 +0200

    get note bug fix

--- .protected/usr/fragment/template/user.php
+++ .protected/usr/fragment/template/user.php
@@ -9,7 +9,7 @@ return function ($user) {
 			'note_save' => WEBSITE_URL . '/user/' . $user . '/note/save',
 			'note_delete' => WEBSITE_URL . '/user/' . $user . '/note/delete',
 			'logout' => WEBSITE_URL . '/logout/' . $user,
-			'note' => WEBSITE_URL . '/user/' . $user . '/note/get'
+			'note_get' => WEBSITE_URL . '/user/' . $user . '/note/get'
 		]
 	];
 };
